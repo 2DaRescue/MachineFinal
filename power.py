a3a99c8d098a5d87ab20644da0af257e1dfe156b
2DaRescue <d.san01@hotmail.com>
2024-11-28 16:54:37 -0700

    testing gpu

--- power.py
+++ power.py
@@ -1,6 +1,7 @@
-
 import tensorflow as tf
-print(tf.__version__)
 
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+
+for gpu in tf.config.list_physical_devices('GPU'):
+    print(gpu)
